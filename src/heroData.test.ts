6b59ae0e0d8344aaf52d11eebbfda2cab62dcedd
Alex Leska <deskmicealex@gmail.com>
2023-04-19 12:30:21 -0700

    Add test to check for heroes with duplicate grid rows

---
+++ src/heroData.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from "vitest";
+import heroData from "./datasets/heroData.json";
+import Hero from "./types/hero";
+
+const createHeroGridEntryHash = (hero: Hero) => {
+  return (
+    hero.lanes.join("") +
+    hero.gender +
+    hero.rangeType +
+    hero.race.join("") +
+    hero.complexity +
+    hero.legs.toString() +
+    hero.releaseYear.toString()
+  );
+};
+
+describe("heroData", () => {
+  it("should not contain any entries with exact matching grid categories", () => {
+    const dupeMap = new Map();
+    const duplicates: Hero[] = [];
+    for (const hero of heroData) {
+      const hash = createHeroGridEntryHash(hero);
+      if (dupeMap.has(hash)) {
+        duplicates.push(dupeMap.get(hash));
+        duplicates.push(hero);
+      }
+      dupeMap.set(hash, hero);
+    }
+    expect(duplicates).toStrictEqual([]);
+  });
+});
